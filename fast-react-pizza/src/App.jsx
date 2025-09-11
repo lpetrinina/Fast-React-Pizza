37b53573219948f56e6a53c3953336c8cf97a67e
Luiza Petrinina <petrinina95@gmail.com>
2025-09-11 11:47:34 +0300

    Fetch menu data
    Fetch data with React Router - loader

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import Order from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
+import { getMenu } from "./services/apiRestaurant.js";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
