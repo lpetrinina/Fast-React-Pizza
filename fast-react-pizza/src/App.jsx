f76ccd5ae37cb75e91678349af0c6ca2894953ae
Luiza Petrinina <petrinina95@gmail.com>
2025-09-11 12:49:21 +0300

    Handle errors with errorElement from React Router

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -6,17 +6,20 @@ import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import Order from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
-import { getMenu } from "./services/apiRestaurant.js";
+import Error from "./ui/Error.jsx";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
+
     children: [
       { path: "/", element: <Home /> },
       {
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
