34f6800e48b4625a777f1faea2de9470953d9c76
Luiza Petrinina <petrinina95@gmail.com>
2025-09-12 11:37:34 +0300

    Set up Tailwind CSS

---
+++ fast-react-pizza/prettier.config.js
@@ -0,0 +1,19 @@
+
+// {
+//     plugins: ["prettier-plugin-tailwindcss",
+
+//     ]
+
+// }
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: false,
+};
+
+module.exports = config;
