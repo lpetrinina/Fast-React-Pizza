26662868e0e9be68f36d5d470a7823c1f288be0b
Luiza Petrinina <petrinina95@gmail.com>
2025-09-16 19:12:13 +0300

    Style the order form

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -34,7 +34,7 @@ const fakeCart = [
 ];
 
 const inputStyle =
-  'w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:ring focus:ring-yellow-400 focus:outline-none md:px-6 md:py-3';
+  'rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:ring focus:ring-yellow-400 focus:outline-none md:px-6 md:py-3';
 
 function CreateOrder() {
   const navigation = useNavigation();
@@ -46,32 +46,53 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       {/* <Form method='POST' action='/order/new'> */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className={inputStyle} type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input
+            className={`${inputStyle} grow`}
+            type="text"
+            name="customer"
+            required
+          />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className={inputStyle} type="tel" name="phone" required />
-            {formErrors?.phone && <p>{formErrors.phone}</p>}
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+
+          <div className="grow">
+            <input
+              className={`${inputStyle} w-full`}
+              type="tel"
+              name="phone"
+              required
+            />
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input className={inputStyle} type="text" name="address" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+
+          <div className="grow">
+            <input
+              className={`${inputStyle} w-full`}
+              type="text"
+              name="address"
+              required
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2 focus:outline-none"
             type="checkbox"
@@ -80,7 +101,7 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">
+          <label htmlFor="priority" className="font-medium">
             Do you want to prioritize your order?
           </label>
         </div>
