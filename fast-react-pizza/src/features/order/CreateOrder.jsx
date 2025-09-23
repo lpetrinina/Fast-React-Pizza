fa26cbb0b2da6a4d0b7539d7797bc902263241bd
Luiza Petrinina <petrinina95@gmail.com>
2025-09-23 10:20:14 +0300

    Use an actual data for creating a new order
    Clear the cart after create a new order

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,37 +3,16 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router';
 import { useSelector } from 'react-redux';
 
 import { createOrder } from '../../services/apiRestaurant';
+import { clearCart, getCart } from '../cart/cartSlice';
 import Button from '../../ui/Button';
+import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str,
   );
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 const inputStyle =
   'rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:ring focus:ring-yellow-400 focus:outline-none md:px-6 md:py-3';
@@ -46,7 +25,11 @@ function CreateOrder() {
   const formErrors = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+
+  if (!cart.length) {
+    return <EmptyCart />;
+  }
 
   return (
     <div className="px-4 py-6">
@@ -145,6 +128,9 @@ export async function action({ request }) {
 
   //   If everything is okay, create a new order and then redirect
   const newOrder = await createOrder(order);
+
+  // Clear cart after create a new order(do NOT overuse this technique)
+  store.dispatch(clearCart());
 
   return redirect(`/order/${newOrder.id}`);
 }
