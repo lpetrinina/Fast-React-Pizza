d60b698a9e908793b8eeb0ed37df3a7e128ecbf0
Luiza Petrinina <petrinina95@gmail.com>
2025-09-24 19:39:57 +0300

    Integrate Geolocation in Order form

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { createOrder } from '../../services/apiRestaurant';
 import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
+import { fetchAddress } from '../user/userSlice';
+import { formatCurrency } from '../../utils/helpers';
+
 import Button from '../../ui/Button';
 import EmptyCart from '../cart/EmptyCart';
 import store from '../../store';
-import { formatCurrency } from '../../utils/helpers';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -20,11 +22,20 @@ const inputStyle =
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === 'loading';
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
+  const dispatch = useDispatch();
 
   const cart = useSelector(getCart);
   const totalCartPrice = useSelector(getTotalCartPrice);
@@ -70,7 +81,7 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:relative sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
 
           <div className="grow">
@@ -78,9 +89,33 @@ function CreateOrder() {
               className={`${inputStyle} w-full`}
               type="text"
               name="address"
+              defaultValue={address}
+              disabled={isLoadingAddress}
               required
             />
+
+            {addressStatus === 'error' && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+
+          {/* Display button only if position not fetched */}
+          {!position.latitude && !position.longitude && !errorAddress && (
+            <span className="absolute right-[3px] bottom-[3px] z-10 sm:right-[3px] md:right-[5px] md:bottom-[5px]">
+              <Button
+                type="small"
+                disabled={isLoadingAddress}
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -99,8 +134,17 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.latitude && position.longitude
+                ? `${position.latitude}, ${position.longitude}`
+                : ''
+            }
+          />
 
-          <Button type="primary" disabled={isSubmitting}>
+          <Button type="primary" disabled={isSubmitting || isLoadingAddress}>
             {isSubmitting
               ? 'Placing order...'
               : `Order now for ${formatCurrency(totalPrice)}`}
@@ -119,6 +163,8 @@ export async function action({ request }) {
     ...data,
     cart: JSON.parse(data.cart),
   };
+
+  console.log(order);
 
   const errors = {};
 
